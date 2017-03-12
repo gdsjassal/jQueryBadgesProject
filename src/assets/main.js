310b1bb1173a498835328a30bd1a12da0a481dbf
Gagan Singh Jassal <gdsjassal.er@live.com>
2017-03-12 10:45:23 -0400

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,7 +5,7 @@ $(function() {
         url : "https://www.codeschool.com/users/Gdsjassal.json",
         dataType: "jsonp",
         success: function (response) {
-            for (var i = 0; i < response.courses.completed[i]; i++) {
+            for (var i = 0; i < response.courses.completed.length; i++) {
                 $("#badges").append('<div class="course"><h3>' + response.courses.completed[i].title + '</h3><img src="' + response.courses.completed[i].badge + '" ><a href="' + response.courses.completed[i].url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
             }
         }
